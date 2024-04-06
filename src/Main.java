f69769b7cfe07ae4eb21e8d8480e490a0b4cd245
werhamburger <awokado100501@gmail.com>
2024-04-07 01:12:50 +0200

    Dodajemy pierwsza lekcje

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Kocham Kamisia!");
+        char a = 'a';
+
+        String kamilPierdzi = "Kamil pierdzi wszystko smierdzi";
+
+        int anniversary = 2307;
+        System.out.println(kamilPierdzi + " " + anniversary);
+
+        int year = 2024;
+        float myBday = 10.05F;
+        double kamisBday = 02.04;
+        short day = 7;
+        long temp = -546766;
+        boolean kamisIsTheHandsomest = true;
+        String  bearKamis = "Kamis to kochany mis";
+        char w = 'w';
+        var duck = "duck";
+        System.out.println("\nRok: " + year + "." +
+                "\nMoje urodziny: " + myBday + "." +
+                "\nUrodziny Kamisia: " + kamisBday + "." +
+                "\nDzień: " + day + "." +
+                "\nTemperatura gdzies na Islandii: " + temp + "." +
+                "\nCzy Kamis jest najpiękniejszy na swiecie? " + kamisIsTheHandsomest +
+                "\nKto to jest Kamis? " + bearKamis +
+                "\nLitera imienia Weronisi: " + w + ".");
+
+    }
+}
